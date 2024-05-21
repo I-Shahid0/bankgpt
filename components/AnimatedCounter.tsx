fc9225436e627d111609ec166e2d9d2dbe96e1e5
Ibrahim Shahid <ibbi14401@gmail.com>
2024-05-20 21:28:46 -0400

    Chart/Layout complete

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+"use client";
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+        <CountUp
+        duration={2.75}
+        end={amount}
+        decimal=","
+        decimals={2}
+        prefix="$"
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
